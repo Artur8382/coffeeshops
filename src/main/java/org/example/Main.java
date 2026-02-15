f66759b5b66b6744e4f7f480d76f11623f814b78
Artur Ardai <krkerdo@gmail.com>
2026-02-15 19:39:26 +0200

    validating inputs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
 package org.example;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.csv.CSVParser;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if(!validateArgs(args)){
+            return;
         }
+        
+
     }
+
+    public static boolean validateArgs(String[] args){
+        if (args.length < 3) {
+            System.out.println("Too few arguments");
+            System.out.println("Usage: java -jar app.jar <user x coordinate> <user y coordinate> <shop data url>");
+            return false;
+        }
+        try {
+            Double.parseDouble(args[0]);
+            Double.parseDouble(args[1]); 
+            new URI(args[2]);
+        } catch (NumberFormatException exception) {
+            System.out.println("First argument must be a decimal number");
+        } catch (URISyntaxException e) {
+            System.out.println("Invalid URL syntax!");
+        }
+        return true;
+    } 
 }
